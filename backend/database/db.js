17036f0c3d9fcfbc5b38dff07f0a722195c8d38d
Alice Phan <nphan03.145@gmail.com>
2022-06-18 20:50:04 -0700

    Create database

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'Seimei98',
+  password: process.env.DATABASE_PASSWORD,
   host: 'localhost',
   port: 5432,
   database: 'diaries'
