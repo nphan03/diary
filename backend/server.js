0b8d4dcb300b4fbfb6b8a8ba5840621b4417f9c6
Alice Phan <nphan03.145@gmail.com>
2022-06-21 23:10:34 -0700

    Create user authentication

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ app.use(cors())
 app.use(express.urlencoded({ extended:true }))
 app.use(express.json())
 
+//users log in
+const usersRouter = require('./routes/users')
+app.use('/', usersRouter)
+
 //get all diaries
 const diariesRouter = require('./routes/diaries')
 app.use('/',diariesRouter)
