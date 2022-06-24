fbb93757e26514c1c07b148b87c378aca7728abe
Alice Phan <nphan03.145@gmail.com>
2022-06-23 19:31:01 -0700

    Create route to modify diary

--- backend/routes/diaries.js
+++ backend/routes/diaries.js
@@ -2,25 +2,11 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../database/db')
 
-//Remove when done
-const { v4: uuidv4 } = require('uuid')
-////////////////
-
-const findDiaryPositionById = async (id, username)=> {
-  try{
-    const getPositionResult = await pool.query(`SELECT position-1 FROM ${username}, jsonb_array_elements(diary_list) with ordinality arr(diary_object, position) WHERE username='${username}' and diary_object->>'id'='${id}';`)
-    const object = getPositionResult.rows[0]
-    return object[Object.keys(object)[0]]
-    
-  }catch (error){
-    return error
-  }
-}
-
 router.route('/:username')
 .get(async (req,res) => {
   try {
-    const allDiary = await pool.query(`SELECT diary_list FROM ${req.params.username}`);
+    const allDiary = await pool.query(`SELECT * FROM ${req.params.username}_diaries;`)
+    
     res.status(201).json(allDiary.rows)
   } catch (error) {
     res.status(500).send(error)
@@ -28,37 +14,34 @@ router.route('/:username')
 })
 .post(async (req,res) => {
   try {
-    //Remove when done
-    const new_diary = { 
-      "id": uuidv4(),
-      ...req.body.diary
-    }
-    ///////////////////////
+    await pool.query(`
+      INSERT INTO ${req.params.username}_diaries (diary_date, diary_text, diary_owner)
+      VALUES ('${req.body.date}', '${req.body.text}', (SELECT user_id FROM users WHERE user_name='${req.params.username}'))
+      RETURNING diary_id;
+    `)
 
-    const diary_list = (await pool.query(`SELECT diary_list FROM ${req.params.username};`))
-    .rows[0].diary_list
-    
-    console.log(`UPDATE ${req.params.username} SET diary_list = diary_list || '${JSON.stringify(new_diary)}' ::jsonb;`)
-
-    if(diary_list != null) {
-      await pool.query(`UPDATE ${req.params.username} SET diary_list = diary_list || '${JSON.stringify(new_diary)}' ::jsonb;`)
-      //await pool.query(`UPDATE ${req.params.username} SET diary_list = diary_list || '${JSON.stringify(req.body.diary)}' ::jsonb;`)
-    } else {
-      await pool.query(`UPDATE ${req.params.username} SET diary_list='[${JSON.stringify(new_diary)}]';`)
-      //await pool.query(`UPDATE ${req.params.username} SET diary_list='[${JSON.stringify(req.body.diary)}]';`)
-    }
     res.status(201).send('Updated diary list')
   }catch (error){
     res.status(500).send(error)
   }
 })
-.put((req,res) => {
-
+.put(async (req,res) => {
+  try{
+    const modifiedDiaryDate = await pool.query(`
+      UPDATE  ${req.params.username}_diaries SET diary_text='${req.body.newText}' WHERE diary_id='${req.body.id}'
+      RETURNING diary_date;
+    `)
+    res.status(201).send(`Updated ${modifiedDiaryDate.rows[0].diary_date} diary`)
+  }catch(error){
+    res.status(500).send(error)
+  }
 })
 .delete(async (req,res) => {
   try{
-    const diary_position = await findDiaryPositionById(req.body.id, req.params.username)
-    await pool.query(`UPDATE ${req.params.username} SET diary_list = diary_list - ${diary_position};`)
+    await pool.query(`
+      DELETE FROM ${req.params.username}_diaries WHERE diary_id='${req.body.id}';
+    `)
+
     res.status(201).send('Updated diary list')
   }catch (error){
     res.status(500).send(error)
